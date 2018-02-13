4f2c89d193d60e83abaaa516f7c293a9fce7c2db
JulianoGTZ <julianogutierrez@hotmail.com>
2018-02-13 20:53:59 -0200

    :construction: Adds a DAO Products

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,10 +1,20 @@
 module.exports = (app) => {
     app.get('/produtos', (req, resp) => {
         var connection = app.infra.connectionFactory();
-        connection.query('select * from produtos', (err, result) => {
-            resp.render('produtos/lista', {lista: result});
+        var produtosBanco = new  app.infra.produtosDAO(connection);
+
+        produtosBanco.lista((erros, resultados) => {
+            resp.render('produtos/lista', {lista: resultados});
         });
         connection.end();
     });
-}
+};
+
+
+// app.get('produtos/remove', () => {
+//     var produto = produtosBanco.carrega(connection, id, callback);
+//     if (produto) {
+//         produtosBanco.remove(produto, callback);
+//     }
+// });
 
