658b578c396622f3e52bacba5643f05344f37c50
JulianoGTZ <julianogutierrez@hotmail.com>
2018-02-12 23:06:11 -0200

    :construction: Adds product query

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,8 +1,17 @@
-
-
 module.exports = (app) => {
     app.get('/produtos', (req, resp) => {
-        resp.render("produtos/lista");
+        var mysql = require('mysql');
+        var connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'admin',
+            database: 'casadocodigo'
+        });
+
+        connection.query('select * from produtos', (err, result) => {
+            resp.send(result);
+        });
+        connection.end();
     });
 }
 
