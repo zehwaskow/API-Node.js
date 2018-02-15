896178be1abd7e0e51c53cdc369843a4aede7ba5
Juliano Pereira Lima <julianodgtz@gmail.com>
2018-02-15 00:39:29 -0200

    :up: Configure the body parser

--- config/express.js
+++ config/express.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var load = require('express-load');
+var bodyParser = require('body-parser');
 
 
 module.exports = function () {
@@ -7,6 +8,8 @@ module.exports = function () {
 
     app.set('view engine', 'ejs');
     app.set('views', './app/views/');
+
+    app.use(bodyParser.urlencoded({extended:true}));
 
     load('routes', {cwd: 'app'}).then('infra').into(app);
 
