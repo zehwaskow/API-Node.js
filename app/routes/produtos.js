28f286f8350a5a334f89bd43894d78b3dc836f11
Juliano Pereira Lima <julianodgtz@gmail.com>
2018-02-15 01:01:37 -0200

    :star: Create function to save products sent by the form

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,20 +1,27 @@
 module.exports = (app) => {
     app.get('/produtos', (req, resp) => {
         var connection = app.infra.connectionFactory();
-        var produtosBanco = new  app.infra.produtosDAO(connection);
+        var produtosDAO = new app.infra.produtosDAO(connection);
 
-        produtosBanco.lista((erros, resultados) => {
+        produtosDAO.lista((erros, resultados) => {
             resp.render('produtos/lista', {lista: resultados});
         });
         connection.end();
     });
+
+    app.get('/produtos/form', function (req, res) {
+        res.render('produtos/form');
+    });
+
+    app.post('/produtos/salva', (req, res) => {
+        var connection = app.infra.connectionFactory();
+        var produto = req.body;
+        var produtosDAO = new app.infra.produtosDAO(connection);
+        produtosDAO.salva(produto,(erros,resultados)=>{
+            res.render('produtos/lista',{lista:resultados});
+        });
+
+    });
 };
 
-
-// app.get('produtos/remove', () => {
-//     var produto = produtosBanco.carrega(connection, id, callback);
-//     if (produto) {
-//         produtosBanco.remove(produto, callback);
-//     }
-// });
 
